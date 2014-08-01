87ac7dabe9502fe887ab00ab0a9a29ec558d077a
Carlos Gutierrez <carlos.gtz.94@gmail.com>
2014-07-31 20:29:15 -0500

    Now we have a register form working.

---
+++ app/src/main/java/com/example/carlosgutierrez/prepayandroid/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.carlosgutierrez.prepayandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by carlosgutierrez on 7/31/14.
+ */
+public class RegisterActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void registerUser(View v){
+        Toast.makeText(this,"Login",Toast.LENGTH_SHORT).show();
+    }
+}
