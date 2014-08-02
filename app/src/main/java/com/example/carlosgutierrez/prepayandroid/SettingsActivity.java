24bc2d165c0ff4a81ceecdbe975fc8fd9156252a
Carlos Gutierrez <carlos.gtz.94@gmail.com>
2014-08-01 19:50:28 -0500

    Now login screen works.

--- app/src/main/java/com/example/carlosgutierrez/prepayandroid/SettingsActivity.java
+++ app/src/main/java/com/example/carlosgutierrez/prepayandroid/SettingsActivity.java
@@ -1,27 +1,24 @@
 package com.example.carlosgutierrez.prepayandroid;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import com.example.carlosgutierrez.prepayandroid.R;
 
-
-public class MainMyActivity extends Activity {
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_my);
+        setContentView(R.layout.activity_settings);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_my, menu);
+        getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
 
@@ -35,16 +32,5 @@ public class MainMyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void doLogin(View v) {
-        startActivity(new Intent(this, LoginActivity.class));
-        //Toast.makeText(this, "It works!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void doRegister(View v) {
-        startActivity(new Intent(this, RegisterActivity.class));
-        //Toast.makeText(this, "It works too!", Toast.LENGTH_SHORT).show();
-
     }
 }
